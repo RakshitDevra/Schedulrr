3738841d0833715398840b2cd0bcc3d2f2cef48a
RakshitDevra <110564848+RakshitDevra@users.noreply.github.com>
2024-10-15 23:34:44 +0530

    Updated Incomplete line of Footer

--- app/page.js
+++ app/page.js
@@ -110,7 +110,7 @@ const page = () => {
         <div className='bg-blue-600 text-white rounded-lg p-8 text-center '>
           <h2 className='text-3xl font-bold mb-4'>Ready To Simplify Your Scheduling?</h2>
           <p className='text-xl mb-6'>
-            Join thousands of professionals who trust Schedulrr for effecient time
+            Join thousands of professionals who trust Schedulrr for effecient time management
           </p>
           <Link href={"/dashboard"}><Button size="lg" variant="secondary" className='text-blue-600'>Start For Free <ArrowRight className='ml-2 h-5 w-5' /></Button></Link>
         </div>
